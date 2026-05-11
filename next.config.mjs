0c9a2a4e58478fcc1ed3f767584f7781bb8e2861
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2026-05-11 08:33:48 +0200

    change site config and patch up tests

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,11 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-const basePath = process.env.SITE_BASE_PATH || "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // GitHub Pages can host static files, but not a running Next server.
     // output: "export" tells Next to generate static HTML/CSS/JS that GitHub Pages can serve.
     output: "export",
-    basePath: basePath, // basePath helps Next route the app under /repo-name
-    assetPrefix: basePath || undefined, // assetPrefix helps Next serve static files from the correct path
     typescript: {
         // ignoreBuildErrors: true,
     },
