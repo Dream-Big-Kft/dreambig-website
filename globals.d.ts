77cdd148fe8c4a2e5ba6dc74d0a81f6740bc1bf3
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2026-05-16 11:10:28 +0200

    type declaration to TypeScript understandig window.Cookiebot?.consent?.statistics without casting

--- globals.d.ts
+++ globals.d.ts
@@ -2,3 +2,14 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+interface Window {
+  Cookiebot?: {
+    consent?: {
+      statistics: boolean;
+      marketing: boolean;
+      preferences: boolean;
+    };
+    renew?: () => void;
+  };
+}
